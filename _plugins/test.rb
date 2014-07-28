f36cb487283e3862993cd5da9221d09dea22ba9d
Svetlana Malakhova <jupiteror1@yahoo.com>
2014-07-28 14:16:14 +0700

    Added generic styles

--- _plugins/test.rb
+++ _plugins/test.rb
@@ -2,21 +2,23 @@ module Jekyll
   class WithinCollectionPostNavigation < Generator
     def generate(site)
       site.collections.each_pair do |label, collection|
-        collection.docs.sort! { |a,b| b <=> a}
-        collection.docs.each do |docs|
-          index = collection.docs.index docs
-          next_in_category = nil
-          previous_in_category = nil
-          if index
-            if index < collection.docs.length - 1
-              next_in_category = collection.docs[index + 1]
+        collection.docs.sort! { |a,b| b.data["order"] <=> a.data["order"] }
+        collection.docs.group_by{|d| d.data["category"]}.each do |cat, docs|
+          docs.each do |doc|
+            index = docs.index doc
+            next_in_category = nil
+            previous_in_category = nil
+            if index
+              if index < docs.length - 1
+                next_in_category = docs[index + 1]
+              end
+              if index > 0
+                previous_in_category = docs[index - 1]
+              end
             end
-            if index > 0
-              previous_in_category = collection.docs[index - 1]
-            end
+            doc.data["next_in_category"] = next_in_category unless next_in_category.nil?
+            doc.data["previous_in_category"] = previous_in_category unless previous_in_category.nil?
           end
-          docs.data["next_in_category"] = next_in_category unless next_in_category.nil?
-          docs.data["previous_in_category"] = previous_in_category unless previous_in_category.nil?
         end
       end
     end
